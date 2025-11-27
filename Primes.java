9e83adc9949e5f3f38e4483e0417fe52b498140b
Ariel Eliahu <ariel.eliahu@post.runi.ac.il>
2025-11-27 18:09:52 +0200

    Completed assignment

--- Primes.java
+++ Primes.java
@@ -3,6 +3,7 @@ public class Primes {
         final int N = Integer.parseInt(args[0]);
         int primeCounter = 0;
         int i = 2;
+        System.out.println("Prime numbers up to " + N + ":");
         if (i % 2 == 0) {
             System.out.println(i);
             i += 1;
@@ -18,8 +19,13 @@ public class Primes {
             i += 2;
             primeCounter += 1;
         }
+        if (i % 7 == 0) {
+            System.out.println(i);
+            i += 2;
+            primeCounter += 1;
+        }
         while (i <= N) {
-            if (i % 2 == 0 || i % 3 == 0 || i % 5 == 0) {
+            if (i % 2 == 0 || i % 3 == 0 || i % 5 == 0 || i % 7 == 0 ) {
             }
             else {
                 System.out.println(i);
@@ -28,7 +34,8 @@ public class Primes {
             i += 2; }
 
         double total_num = N;
-        double percentage = Math.ceil((primeCounter/(total_num)*100) -1);
-        System.out.println("There are " + primeCounter + " primes between 2 and " + N + " (" + percentage + "% are primes)");
+        double percentage = (primeCounter/(total_num)*100);
+        int perc = (int) Math.round(percentage);
+        System.out.println("There are " + primeCounter + " primes between 2 and " + N + " (" + perc + "% are primes)");
     }
 }
